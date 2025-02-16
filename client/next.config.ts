f558bdb3f7dbbcb62cea45812e1cc689d5475446
Godswill Effi <godswilleffi@gmail.com>
2025-02-16 05:30:45 -0600

    Slight change

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  api: {
-    bodyParser: true, // Ensure API routes process JSON payloads
-  },
 };
 
 export default nextConfig;
