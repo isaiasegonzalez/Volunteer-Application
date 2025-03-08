d33f92dc14e99509e7e2f81ed8a917521bcb07ca
Godswill Effi <godswilleffi@gmail.com>
2025-03-08 00:30:52 -0600

    Fixed SWC and Babel Issue

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,6 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
+  experimental: {
+    forceSwcTransforms: true, // Still forces SWC usage
+  },
 };
 
 export default nextConfig;
